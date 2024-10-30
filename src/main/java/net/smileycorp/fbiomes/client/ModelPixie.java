3e965628eed3877653c6e587a9a545100045cccb
Smileycorp <callum.j.longbottom@gmail.com>
2024-10-30 06:21:25 +0000

    finish pixie rendering v1

--- src/main/java/net/smileycorp/fbiomes/client/ModelPixie.java
+++ src/main/java/net/smileycorp/fbiomes/client/ModelPixie.java
@@ -1,58 +1,39 @@
 package net.smileycorp.fbiomes.client;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBox;
 import net.minecraft.client.model.ModelRenderer;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 
 public class ModelPixie extends ModelBase {
     
-    private final RenderManager rm;
-    private final ModelRenderer body;
     private final ModelRenderer l_wing;
     private final ModelRenderer r_wing;
     
-    public ModelPixie(RenderManager rm) {
+    public ModelPixie() {
         textureWidth = 16;
         textureHeight = 16;
-        this.rm = rm;
-        body = new ModelRenderer(this).addBox(-3, -6, 0, 6, 6, 0, false);
-        l_wing = new ModelRenderer(this).addBox(-2, -7, 0, 0, 4, 6, false);
-        r_wing = new ModelRenderer(this).addBox(2, -7, 0, 0, 4, 6, true);
+        l_wing = new ModelRenderer(this);
+        l_wing.setRotationPoint(-2, 19, 0.0F);
+        l_wing.cubeList.add(new ModelBox(l_wing, 0, 0, 0, 0, 0, 0, 5, 6, 0, true));
+        l_wing.rotateAngleX = 0.6108652f;
+        r_wing = new ModelRenderer(this);
+        r_wing.setRotationPoint(2, 19, 0);
+        r_wing.cubeList.add(new ModelBox(r_wing, 0, 0, 0, 0, 0, 0, 5, 6, 0, false));
+        r_wing.rotateAngleX = 0.6108652f;
     }
     
     @Override
     public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entity) {
-        float rotation = (float) (0.45f * (1 + Math.sin(ageInTicks)));
+        float rotation = (float) (0.45f * (1 + Math.sin(ageInTicks * 0.6f)));
         l_wing.rotateAngleY = -rotation;
         r_wing.rotateAngleY = rotation;
     }
     
     @Override
     public void render(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-        scale = scale * 0.5f;
         l_wing.render(scale);
         r_wing.render(scale);
-        GlStateManager.pushMatrix();
-        RenderHelper.enableStandardItemLighting();
-        GlStateManager.rotate(180.0F + interpolateRotation((EntityLivingBase) entity, Minecraft.getMinecraft().getRenderPartialTicks()), 0.0F, 1.0F, 0.0F);
-        GlStateManager.rotate(180.0F - rm.playerViewY, 0.0F, 1.0F, 0.0F);
-        GlStateManager.rotate((float)(rm.options.thirdPersonView == 2 ? -1 : 1) * -rm.playerViewX, 1.0F, 0.0F, 0.0F);
-        body.render(scale);
-        GlStateManager.disableBlend();
-        GlStateManager.disableRescaleNormal();
-        GlStateManager.popMatrix();
-    }
-    
-    protected float interpolateRotation(EntityLivingBase entity, float partialTicks) {
-        float f;
-        for (f = entity.renderYawOffset - entity.prevRenderYawOffset; f < -180.0F; f += 360.0F);
-        while (f >= 180.0F) f -= 360.0F;
-        return entity.prevRenderYawOffset + partialTicks * f;
     }
     
 }
--- src/main/java/net/smileycorp/fbiomes/client/RenderPixie.java
+++ src/main/java/net/smileycorp/fbiomes/client/RenderPixie.java
@@ -1,7 +1,9 @@
 package net.smileycorp.fbiomes.client;
 
+import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
 import net.smileycorp.fbiomes.common.Constants;
 import net.smileycorp.fbiomes.common.entities.EntityPixie;
@@ -11,7 +13,45 @@ import javax.annotation.Nullable;
 public class RenderPixie extends RenderLiving<EntityPixie> {
     
     public RenderPixie(RenderManager rm) {
-        super(rm, new ModelPixie(rm), 0.4F);
+        super(rm, new ModelPixie(), 0.4F);
+    }
+    
+    @Override
+    public void doRender(EntityPixie entity, double x, double y, double z, float yaw, float pt) {
+        if (!renderOutlines) {
+            GlStateManager.pushMatrix();
+            GlStateManager.translate((float)x, (float)y, (float)z);
+            bindEntityTexture(entity);
+            RenderHelper.enableStandardItemLighting();
+            final float u0 = 0;
+            final float v0 = 0F;
+            final float u1 = 0.375f;
+            final float v1 = 0.375f;
+            int brightness = entity.getBrightnessForRender();
+            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, brightness % 65536, brightness / 65536);
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            GlStateManager.translate(0.0F, 0.1F, 0.0F);
+            GlStateManager.rotate(180.0F - renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
+            GlStateManager.rotate((float)(renderManager.options.thirdPersonView == 2 ? -1 : 1) * -renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+            float scale = 0.4F;
+            GlStateManager.scale(scale, scale, scale);
+            GlStateManager.enableNormalize();
+            GlStateManager.enableBlend();
+            GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+            Tessellator tesselator = Tessellator.getInstance();
+            BufferBuilder buffer = tesselator.getBuffer();
+            buffer.begin(7, DefaultVertexFormats.POSITION_TEX_NORMAL);
+            buffer.pos(-0.5, -0.25, 0.0).tex(u0, v1).normal(0.0F, 1.0F, 0.0F).endVertex();
+            buffer.pos(0.5, -0.25, 0.0).tex(u1, v1).normal(0.0F, 1.0F, 0.0F).endVertex();
+            buffer.pos(0.5, 0.75, 0.0).tex(u1, v0).normal(0.0F, 1.0F, 0.0F).endVertex();
+            buffer.pos(-0.5, 0.75, 0.0).tex(u0, v0).normal(0.0F, 1.0F, 0.0F).endVertex();
+            tesselator.draw();
+            GlStateManager.disableBlend();
+            GlStateManager.disableNormalize();
+            GlStateManager.disableRescaleNormal();
+            GlStateManager.popMatrix();
+        }
+        super.doRender(entity, x, y, z, yaw, pt);
     }
     
     @Nullable
