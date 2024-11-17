53fff6423219eb9f024866e4ccb25c560b3f56c7
Smileycorp <callum.j.longbottom@gmail.com>
2024-11-17 19:18:14 +0000

    fix mushroom placing

--- src/main/java/net/smileycorp/fbiomes/common/blocks/BlockFBMushroom.java
+++ src/main/java/net/smileycorp/fbiomes/common/blocks/BlockFBMushroom.java
@@ -109,7 +109,7 @@ public class BlockFBMushroom extends BlockBush implements IGrowable, BlockProper
     }
 	 
 	protected boolean canSustain(IBlockAccess world, BlockPos pos, EnumFacing facing) {
-		IBlockState state = world.getBlockState(pos.offset(facing));
+		IBlockState state = world.getBlockState(pos.offset(facing.getOpposite()));
 		return (state.isFullCube() && state.getBlock() instanceof BlockLog) ||
 				state.getBlock().canSustainPlant(state, world, pos, facing, this);
 	}
