8d8014d444ea1f989b879bea888f300a89dfe737
Smileycorp <callum.j.longbottom@gmail.com>
2024-11-19 16:35:32 +0000

    change big glowshroom shape

--- src/main/java/net/smileycorp/fbiomes/common/world/gen/fungusforest/WorldGenBigFBMushroom.java
+++ src/main/java/net/smileycorp/fbiomes/common/world/gen/fungusforest/WorldGenBigFBMushroom.java
@@ -2,63 +2,85 @@ package net.smileycorp.fbiomes.common.world.gen.fungusforest;
 
 import com.google.common.collect.Lists;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
 import net.smileycorp.fbiomes.common.blocks.BlockBigMushroom;
 import net.smileycorp.fbiomes.common.blocks.FBiomesBlocks;
 import net.smileycorp.fbiomes.common.blocks.enums.EnumMushroomShape;
 import net.smileycorp.fbiomes.common.blocks.enums.EnumMushroomVariant;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 
-public class WorldGenBigFBMushroom extends WorldGenBigMushroomBase {
+public class WorldGenBigFBMushroom extends WorldGenerator {
 	
-	protected IBlockState spot;
-	
-	List<Vec3i> spots = Lists.newArrayList();
+	protected final IBlockState stem;
+	protected final IBlockState cap;
+	protected final IBlockState spot;
 	
 	public WorldGenBigFBMushroom(Random rand) {
-		super(rand);
+		this(EnumMushroomVariant.values()[rand.nextInt(EnumMushroomVariant.values().length)]);
 	}
 	
 	public WorldGenBigFBMushroom(EnumMushroomVariant type) {
-		super(FBiomesBlocks.BIG_SHROOM.getDefaultState().withProperty(BlockBigMushroom.SHAPE, EnumMushroomShape.STEM).withProperty(BlockBigMushroom.VARIANT, type)
-				, FBiomesBlocks.BIG_SHROOM.getDefaultState().withProperty(BlockBigMushroom.SHAPE, EnumMushroomShape.CAP).withProperty(BlockBigMushroom.VARIANT, type));
+		super();
+		stem = FBiomesBlocks.BIG_SHROOM.getDefaultState().withProperty(BlockBigMushroom.SHAPE, EnumMushroomShape.STEM).withProperty(BlockBigMushroom.VARIANT, type);
+		cap = FBiomesBlocks.BIG_SHROOM.getDefaultState().withProperty(BlockBigMushroom.SHAPE, EnumMushroomShape.CAP).withProperty(BlockBigMushroom.VARIANT, type);
 		spot = FBiomesBlocks.BIG_SHROOM.getDefaultState().withProperty(BlockBigMushroom.SHAPE, EnumMushroomShape.SPOT).withProperty(BlockBigMushroom.VARIANT, type);
 	}
 	
 	@Override
-	protected void pickType(Random rand) {
-		EnumMushroomVariant variant = EnumMushroomVariant.values()[rand.nextInt(EnumMushroomVariant.values().length)];
-		stem = FBiomesBlocks.BIG_SHROOM.getDefaultState().withProperty(BlockBigMushroom.SHAPE, EnumMushroomShape.STEM).withProperty(BlockBigMushroom.VARIANT, variant);
-		cap = FBiomesBlocks.BIG_SHROOM.getDefaultState().withProperty(BlockBigMushroom.SHAPE, EnumMushroomShape.CAP).withProperty(BlockBigMushroom.VARIANT, variant);
-		spot = FBiomesBlocks.BIG_SHROOM.getDefaultState().withProperty(BlockBigMushroom.SHAPE, EnumMushroomShape.SPOT).withProperty(BlockBigMushroom.VARIANT, variant);
-	}
-	
-	@Override
-	public void placeCap(World world, Random rand, BlockPos startPos, BlockPos currentPos, int height) {
-		IBlockState state = world.getBlockState(currentPos);
-		if (state.getBlock().canBeReplacedByLeaves(state, world, currentPos)) {
-			if (spots.isEmpty()) genSpots(rand);
-			Vec3i vec = new Vec3i(currentPos.getX()-startPos.getX(), 0, currentPos.getZ()-startPos.getZ());
-			if (spots.contains(vec)) setBlockAndNotifyAdequately(world, currentPos, spot);
-			else setBlockAndNotifyAdequately(world, currentPos, cap);
-		}
-	}
-
-	private void genSpots(Random rand) {
-		int count = rand.nextInt(3)+3;
+	public boolean generate(World world, Random rand, BlockPos pos) {
+		ArrayList<Vec3i> spots = Lists.newArrayList();
+		int count = rand.nextInt(3) + 3;
 		for (int i = 0; i < count; i++) {
 			int x = 2;
 			int z = 2;
-			while ((Math.abs(x)+Math.abs(z))>3 && !spots.contains(new Vec3i(x, 0, z))) {
-				x = rand.nextInt(5)-2;
-				z = rand.nextInt(5)-2;
+			while ((Math.abs(x) + Math.abs(z)) > 3 && !spots.contains(new Vec3i(x, 0, z))) {
+				x = rand.nextInt(5 ) - 2;
+				z = rand.nextInt(5) - 2;
 			}
 			spots.add(new Vec3i(x, 0, z));
+		}
+		int height = rand.nextInt(3) + 2;
+		IBlockState soil = world.getBlockState(pos.down());
+		if (!soil.getBlock().canSustainPlant(soil, world, pos.down(), EnumFacing.UP, FBiomesBlocks.TOADSTOOL)) return false;
+		BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos(pos);
+		for (int j = 0; j <= height + 1; j++) {
+			IBlockState state = world.getBlockState(mutable);
+			mutable.move(EnumFacing.UP);
+			if (!state.getBlock().canBeReplacedByLeaves(state, world, mutable)) return false;
+		}
+		BlockPos startPos = pos.up(height + 1);
+		mutable.setPos(pos.getX(), pos.getY(), pos.getZ());
+		//stem
+		for (int j = 0; j <= height; j++) {
+			setBlockAndNotifyAdequately(world, mutable, stem);
+			mutable.move(EnumFacing.UP);
+		}
+		//cap
+		for (int i = -2; i < 3; i++) {
+			for (int k = -2; k < 3; k++) {
+				mutable.setPos(pos.getX() + i, pos.getY() + height + 1, pos.getZ() + k);
+				double distance = startPos.getDistance(mutable.getX(), mutable.getY(), mutable.getZ());
+				if (distance < 1.5) placeCap(world, startPos, mutable, spots);
+				else if (distance < 2.5) placeCap(world, startPos, mutable.move(EnumFacing.DOWN), spots);
+			}
+		}
+		return true;
+	}
+	
+	public void placeCap(World world, BlockPos startPos, BlockPos currentPos, List<Vec3i> spots) {
+		IBlockState state = world.getBlockState(currentPos);
+		if (state.getBlock().canBeReplacedByLeaves(state, world, currentPos)) {
+			Vec3i vec = new Vec3i(currentPos.getX() - startPos.getX(), 0, currentPos.getZ() - startPos.getZ());
+			if (spots.contains(vec)) setBlockAndNotifyAdequately(world, currentPos, spot);
+			else setBlockAndNotifyAdequately(world, currentPos, cap);
 		}
 	}
 
--- src/main/java/net/smileycorp/fbiomes/common/world/gen/fungusforest/WorldGenBigGlowshroom.java
+++ src/main/java/net/smileycorp/fbiomes/common/world/gen/fungusforest/WorldGenBigGlowshroom.java
@@ -1,5 +1,10 @@
 package net.smileycorp.fbiomes.common.world.gen.fungusforest;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
 import net.smileycorp.fbiomes.common.blocks.BlockBigGlowshroom;
 import net.smileycorp.fbiomes.common.blocks.FBiomesBlocks;
 import net.smileycorp.fbiomes.common.blocks.enums.EnumGlowshroomVariant;
@@ -8,22 +13,56 @@ import net.smileycorp.fbiomes.common.blocks.enums.EnumMushroomShape;
 import java.util.Random;
 
 
-public class WorldGenBigGlowshroom extends WorldGenBigMushroomBase {
-
+public class WorldGenBigGlowshroom extends WorldGenerator {
+	
+	protected final IBlockState stem;
+	protected final IBlockState cap;
+	
 	public WorldGenBigGlowshroom(Random rand) {
-		super(rand);
+		this(EnumGlowshroomVariant.values()[rand.nextInt(EnumGlowshroomVariant.values().length)]);
 	}
-
+	
 	public WorldGenBigGlowshroom(EnumGlowshroomVariant type) {
-		super(FBiomesBlocks.BIG_GLOWSHROOM.getDefaultState().withProperty(BlockBigGlowshroom.SHAPE, EnumMushroomShape.STEM).withProperty(BlockBigGlowshroom.VARIANT, type),
-				FBiomesBlocks.BIG_GLOWSHROOM.getDefaultState().withProperty(BlockBigGlowshroom.SHAPE, EnumMushroomShape.CAP).withProperty(BlockBigGlowshroom.VARIANT, type));
+		super();
+		stem = FBiomesBlocks.BIG_GLOWSHROOM.getDefaultState().withProperty(BlockBigGlowshroom.SHAPE, EnumMushroomShape.STEM).withProperty(BlockBigGlowshroom.VARIANT, type);
+		cap = FBiomesBlocks.BIG_GLOWSHROOM.getDefaultState().withProperty(BlockBigGlowshroom.SHAPE, EnumMushroomShape.CAP).withProperty(BlockBigGlowshroom.VARIANT, type);
 	}
-
+	
 	@Override
-	protected void pickType(Random rand) {
-		EnumGlowshroomVariant variant = EnumGlowshroomVariant.values()[rand.nextInt(EnumGlowshroomVariant.values().length)];
-		stem = FBiomesBlocks.BIG_GLOWSHROOM.getDefaultState().withProperty(BlockBigGlowshroom.SHAPE, EnumMushroomShape.STEM).withProperty(BlockBigGlowshroom.VARIANT, variant);
-		cap = FBiomesBlocks.BIG_GLOWSHROOM.getDefaultState().withProperty(BlockBigGlowshroom.SHAPE, EnumMushroomShape.CAP).withProperty(BlockBigGlowshroom.VARIANT, variant);
+	public boolean generate(World world, Random rand, BlockPos pos) {
+		int height = rand.nextInt(3) + 3;
+		IBlockState soil = world.getBlockState(pos.down());
+		if (!soil.getBlock().canSustainPlant(soil, world, pos.down(), EnumFacing.UP, FBiomesBlocks.TOADSTOOL)) return false;
+		BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos(pos);
+		for (int j = 0; j <= height + 1; j++) {
+			IBlockState state = world.getBlockState(mutable);
+			mutable.move(EnumFacing.UP);
+			if (!state.getBlock().canBeReplacedByLeaves(state, world, mutable)) return false;
+		}
+		mutable.setPos(pos.getX(), pos.getY(), pos.getZ());
+		//stem
+		for (int j = 0; j <= height; j++) {
+			setBlockAndNotifyAdequately(world, mutable, stem);
+			mutable.move(EnumFacing.UP);
+		}
+		//cap
+		for (int j = 0; j < 4; j++) {
+			for (int i = -2; i < 3; i++) {
+				int absi = Math.abs(i);
+				if (j > 1 && absi == 2) continue;
+				for (int k = -2; k <= 2; k++) {
+					int absk = Math.abs(k);
+					if (j < 2 && absi < 2 && absk < 2) continue;
+					if (j > 1 && absk == 2) continue;
+					if (absi + absk == 4) continue;
+					if (j == 3 && absi + absk > 1) continue;
+					mutable.setPos(pos.getX() + i, pos.getY() + j + height - 2, pos.getZ() + k);
+					IBlockState state = world.getBlockState(mutable);
+					if (state.getBlock().canBeReplacedByLeaves(state, world, mutable)) setBlockAndNotifyAdequately(world, mutable, cap);
+				}
+			}
+		}
+		return true;
 	}
 	
 }
