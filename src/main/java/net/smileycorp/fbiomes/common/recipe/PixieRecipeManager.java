a6dfddbabf1e48d104e45807159ba9dfe438bfbf
Smileycorp <callum.j.longbottom@gmail.com>
2025-04-05 23:59:36 +0100

    jei food integration

--- src/main/java/net/smileycorp/fbiomes/common/recipe/PixieRecipeManager.java
+++ src/main/java/net/smileycorp/fbiomes/common/recipe/PixieRecipeManager.java
@@ -1,5 +1,8 @@
 package net.smileycorp.fbiomes.common.recipe;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.ResourceLocation;
@@ -19,6 +22,12 @@ public class PixieRecipeManager {
         for (IRecipe recipe : recipes.values()) if(recipe.matches(inventory.getCraftingWrapper(), world)) return recipe;
         for (IRecipe recipe : CraftingManager.REGISTRY) if(recipe.matches(inventory.getCraftingWrapper(), world)) return recipe;
         return null;
+    }
+    
+    public static float getFoodEfficiency(ItemStack stack) {
+        Item item = stack.getItem();
+        return item instanceof ItemFood ?  1 + 0.01f * (float) ((ItemFood) item).getHealAmount(stack)
+                * (1 + 0.5f * ((ItemFood) item).getSaturationModifier(stack)) : 1;
     }
     
 }
--- src/main/java/net/smileycorp/fbiomes/integration/jei/JeiIntegration.java
+++ src/main/java/net/smileycorp/fbiomes/integration/jei/JeiIntegration.java
@@ -1,8 +1,7 @@
 package net.smileycorp.fbiomes.integration.jei;
 
-import mezz.jei.api.IModPlugin;
-import mezz.jei.api.IModRegistry;
-import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.*;
+import mezz.jei.api.recipe.IRecipeCategoryRegistration;
 import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
 import net.minecraft.item.ItemStack;
 import net.smileycorp.fbiomes.common.blocks.FBiomesBlocks;
@@ -11,8 +10,18 @@ import net.smileycorp.fbiomes.common.blocks.FBiomesBlocks;
 public class JeiIntegration implements IModPlugin {
     
     @Override
+    public void registerCategories(IRecipeCategoryRegistration registry) {
+        IJeiHelpers helpers = registry.getJeiHelpers();
+        IGuiHelper guiHelper = helpers.getGuiHelper();
+        registry.addRecipeCategories(new PixieTableFoodRecipeCategory(guiHelper));
+    }
+    
+    @Override
     public void register(IModRegistry registry) {
-        registry.addRecipeCatalyst(new ItemStack(FBiomesBlocks.MYSTIC_STUMP), VanillaRecipeCategoryUid.CRAFTING);
+        registry.addRecipeCatalyst(new ItemStack(FBiomesBlocks.MYSTIC_STUMP, 1, 1), VanillaRecipeCategoryUid.CRAFTING,
+                PixieTableFoodRecipeCategory.ID);
+        registry.handleRecipes(PixieTableFoodRecipeCategory.Wrapper.class, r -> r, PixieTableFoodRecipeCategory.ID);
+        registry.addRecipes(PixieTableFoodRecipeCategory.getRecipes(), PixieTableFoodRecipeCategory.ID);
     }
     
 }
