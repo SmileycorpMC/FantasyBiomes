6e1c02b06dded0e049dc8a595c76166f1ddca0f1
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-01-31 21:37:21 +0000

    fix mobs spawning in the enchanted thicket

--- src/main/java/net/smileycorp/phantasiai/common/EventListener.java
+++ src/main/java/net/smileycorp/phantasiai/common/EventListener.java
@@ -2,6 +2,7 @@ package net.smileycorp.phantasiai.common;
 
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
@@ -11,12 +12,12 @@ import net.smileycorp.phantasiai.common.world.biomes.PhantasiaiBiomes;
 public class EventListener {
     
     @SubscribeEvent
-    public void onSpawn(LivingSpawnEvent.CheckSpawn event) {
-        if ((((ICreatureType)event.getEntity()).getCreatureType()) == null) return;
-        if (((ICreatureType)event.getEntity()).getCreatureType().getPeacefulCreature()) return;
-        if (event.isSpawner()) return;
-        if (event.getY() < 65) return;
-        if (event.getWorld().getBiome(new BlockPos(event.getEntity())) == PhantasiaiBiomes.ENCHANTED_THICKET) event.setResult(Event.Result.DENY);
+    public void onSpawn(WorldEvent.PotentialSpawns event) {
+        if (event.getType() == null) return;
+        if (event.getType().getPeacefulCreature()) return;
+        BlockPos pos = event.getPos();
+        if (pos.getY() < 65) return;
+        if (event.getWorld().getBiome(pos) == PhantasiaiBiomes.ENCHANTED_THICKET) event.setCanceled(true);
     }
 
     @SubscribeEvent
