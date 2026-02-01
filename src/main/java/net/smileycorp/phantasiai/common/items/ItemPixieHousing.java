c980c7bdacc5c5068bd51cd470ae2944c7c5a108
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-02-01 12:27:52 +0000

    add pixie housing tooltip

--- src/main/java/net/smileycorp/phantasiai/common/items/ItemPixieHousing.java
+++ src/main/java/net/smileycorp/phantasiai/common/items/ItemPixieHousing.java
@@ -1,9 +1,15 @@
 package net.smileycorp.phantasiai.common.items;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
 import net.smileycorp.phantasiai.common.Constants;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemPixieHousing extends ItemFBiomes {
     
@@ -34,5 +40,9 @@ public class ItemPixieHousing extends ItemFBiomes {
     public String byMeta(int meta) {
         return VARIANTS[meta % VARIANTS.length];
     }
-    
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World world, List<String> lines, ITooltipFlag flag) {
+        lines.add(new TextComponentTranslation(getUnlocalizedName(stack) + ".tooltip").getFormattedText());
+    }
 }
