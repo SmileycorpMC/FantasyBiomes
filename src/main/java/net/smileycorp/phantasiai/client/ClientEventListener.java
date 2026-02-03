337579f6baf4f3718bf84b260ef786fac4a91162
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-02-03 16:42:38 +0000

    enchanted thicket particles

--- src/main/java/net/smileycorp/phantasiai/client/ClientEventListener.java
+++ src/main/java/net/smileycorp/phantasiai/client/ClientEventListener.java
@@ -2,7 +2,9 @@ package net.smileycorp.phantasiai.client;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.biome.Biome;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -14,20 +16,29 @@ import net.smileycorp.phantasiai.common.world.biomes.PhantasiaiBiomes;
 
 import java.util.Random;
 
-@Mod.EventBusSubscriber(modid= Constants.MODID, value = Side.CLIENT)
+@Mod.EventBusSubscriber(modid = Constants.MODID, value = Side.CLIENT)
 public class ClientEventListener {
 
     @SubscribeEvent
     public static void clientTick(TickEvent.ClientTickEvent event) {
-        EntityPlayerSP player = Minecraft.getMinecraft().player;
+        if (event.phase != TickEvent.Phase.START) return;
+        Minecraft mc = Minecraft.getMinecraft();
+        if (mc.isGamePaused()) return;
+        EntityPlayerSP player = mc.player;
         if (player == null) return;
-        if (player.world.getBiome(player.getPosition()) != PhantasiaiBiomes.DEAD_MARSH) return;
         Random rand = player.getRNG();
-        Vec3d pos = new Vec3d(player.posX + (rand.nextFloat() - 0.5) * 10, player.posY + rand.nextFloat() * 3,
-                player.posZ + (rand.nextFloat() - 0.5) * 10);
-        Vec3d vec = DirectionUtils.getDirectionVec(pos, new Vec3d(player.posX, player.posY + player.getEyeHeight(), player.posZ));
-        ClientHandler.spawnParticle(EnumParticle.PIXEL, pos.x, pos.y, pos.z, 0d, 20d,
-                rand.nextFloat() * vec.x, rand.nextFloat() * vec.y, rand.nextFloat() * vec.z, 0.5);
+        Biome biome = player.world.getBiome(player.getPosition());
+        if (biome == PhantasiaiBiomes.DEAD_MARSH) {
+            Vec3d pos = new Vec3d(player.posX + (rand.nextFloat() - 0.5) * 10, player.posY + rand.nextFloat() * 3,
+                    player.posZ + (rand.nextFloat() - 0.5) * 10);
+            Vec3d vec = DirectionUtils.getDirectionVec(pos, new Vec3d(player.posX, player.posY + player.getEyeHeight(), player.posZ));
+            ClientHandler.spawnParticle(EnumParticle.PIXEL, pos.x, pos.y, pos.z, 0d, 20d,
+                    rand.nextFloat() * vec.x, rand.nextFloat() * vec.y, rand.nextFloat() * vec.z, 0.5);
+        }
+        if (biome == PhantasiaiBiomes.ENCHANTED_THICKET) {
+            ClientHandler.spawnParticle(EnumParticle.PIXEL, player.posX + (rand.nextFloat() - 0.5) * 10, player.posY + 5 + rand.nextFloat() * 3,
+                    player.posZ + (rand.nextFloat() - 0.5) * 10, (double)0x570300, 60d, (rand.nextFloat() - 0.5f) * 0.2, - (rand.nextFloat() + 0.5) * 0.2,  (rand.nextFloat() - 0.5f) * 0.2, 0.5);
+        }
     }
 
 }
